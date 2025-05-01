2a027ab558e7551f1d3e22361f34c7d4acdadb45
Yan <yaniknezhin@gmail.com>
2025-05-01 23:08:24 +0500

    add app methods

--- app/nocopy.go
+++ app/nocopy.go
@@ -1,6 +1,24 @@
 package app
 
+import (
+	"github.com/aitu-leetcode-site/core/config"
+	"github.com/aitu-leetcode-site/core/grpc"
+	"github.com/aitu-leetcode-site/core/http"
+)
+
 type noCopy struct{}
 
 func (*noCopy) Lock()   {}
 func (*noCopy) Unlock() {}
+
+func (a *App) HttpServer() *http.Server {
+	return a.httpServer
+}
+
+func (a *App) Config() *config.Config {
+	return a.cfg
+}
+
+func (a *App) GrpcServer() *grpc.GRPSServer {
+	return a.grpcServer
+}
